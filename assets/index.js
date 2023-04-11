5dadf0cfbe7608b91559eb7af28b4ff793dcf057
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-04-11 11:40:41 -0700

    OMDB API added

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+//This JavaScript code is designed to fetch and display a list of movies based on a user's search query using the OMDb API.
+
+
+// API endpoint
+// API endpoint: The apiUrl variable holds the base URL for the OMDb API, which is used to fetch movie data.
+const apiUrl = "https://www.omdbapi.com/?apikey=8e83ab41";
